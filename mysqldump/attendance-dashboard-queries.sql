66f15991919284665803454fb98fd1fd2af4f4e3
shreeshyadav <52309936+shreeshyadav@users.noreply.github.com>
2020-06-27 21:53:57 +0530

    adding dashboard queries

--- mysqldump/attendance-dashboard-queries.sql
+++ mysqldump/attendance-dashboard-queries.sql
@@ -1,29 +1,49 @@
-create table present_table as (select student_class_map_id,concat(year(attendance_date),month(attendance_date)) as month_name,count(*) as present_days from student_attendance_map where status='P' group by 1,2 order by 1,2);
+create table present_table_final as (select student_class_map_id,concat(year(attendance_date),month(attendance_date)) as month_name,count(*) as present_days from student_attendance_map where status='P' group by 1,2 order by 1,2);
 
 create table working_table as (select student_class_map_id,concat(year(attendance_date),month(attendance_date)) as month_name,count(*) as working_days from student_attendance_map group by 1,2 order by 1,2);
 
-create table attendance_records select wt.student_class_map_id,wt.month_name,pt.present_days,wt.working_days from present_table pt join working_table wt on pt.student_class_map_id=wt.student_class_map_id and wt.month_name=pt.month_name;
+create table temp as (select student_class_map_id, month_name from working_table)
 
-create table cumulative_attendance_records select ar.student_class_map_id,ar.month_name,sum(at.present_days),sum(at.working_days) from attendance_records ar join attendance_records at on ar.student_class_map_id=at.student_class_map_id and ar.month_name>=at.month_name group by 1,2;
+create table absent_table as (select student_class_map_id,concat(year(attendance_date),month(attendance_date)) as month_name,count(*) as absent_days from student_attendance_map where status='A' group by 1,2 order by 1,2)
 
-#student-attendance-for-each-month
-select month_name,(car.cumulative_present/car.cumulative_working)*100 as attendance_percent from student_class_mapping scm join cumulative_attendance_records car on scm.id=car.student_class_map_id where scm.student_id=253;
+create table absent_table_final as (select t.student_class_map_id, t.month_name, att.absent_days from temp t left OUTER join absent_table att on t.student_class_map_id=att.student_class_map_id and t.month_name=att.month_name order by 1,2)
 
-#best-performer-student_class_map_id
+update absent_table_final set absent_days=0 where absent_days is null
 
-select car.student_class_map_id from student_class_mapping scm join cumulative_attendance_records car on scm.id=car.student_class_map_id where scm.class_id in (select class_id from student_class_mapping where student_id=253) and car.month_name in (select max(month_name) from cumulative_attendance_records) order by cumulative_present desc limit 1;
+create table late_table as (select student_class_map_id,concat(year(attendance_date),month(attendance_date)) as month_name,count(*) as late_days from student_attendance_map where status='L' group by 1,2 order by 1,2)
 
-select month_name,(cumulative_present/cumulative_working)*100 as attendance_percent from cumulative_attendance_records where student_class_map_id in (7);
+create table late_table_final as (select t.student_class_map_id, t.month_name, att.late_days from temp t left OUTER join late_table att on t.student_class_map_id=att.student_class_map_id and t.month_name=att.month_name order by 1,2)
 
-#worst-performer-student_class_map_id
+update late_table_final set absent_days=0 where late_days is null
 
-select car.student_class_map_id from student_class_mapping scm join cumulative_attendance_records car on scm.id=car.student_class_map_id where scm.class_id in (select class_id from student_class_mapping where student_id=253) and car.month_name in (select max(month_name) from cumulative_attendance_records) order by cumulative_present limit 1;
+create table attendance_records_final select wt.student_class_map_id,wt.month_name,pt.present_days,at.absent_days,lt.late_days,wt.working_days from present_table_final pt join working_table wt on pt.student_class_map_id=wt.student_class_map_id and wt.month_name=pt.month_name join absent_table_final at on pt.student_class_map_id=at.student_class_map_id and at.month_name=pt.month_name join late_table_final lt on pt.student_class_map_id=lt.student_class_map_id and lt.month_name=pt.month_name
 
-select month_name,(cumulative_present/cumulative_working)*100 as attendance_percent from cumulative_attendance_records where student_class_map_id in (1);
+create table cumulative_attendance_records_final select ar.student_class_map_id,ar.month_name,sum(at.present_days) as cumulative_present,sum(at.absent_days) as cumulative_absent,sum(at.late_days) as cumulative_late,sum(at.working_days) as cumulative_working from attendance_records_final ar join attendance_records_final at on ar.student_class_map_id=at.student_class_map_id and ar.month_name>=at.month_name group by 1,2;
 
-#avg-class-performance -teacher-view
+drop table temp
 
-select month_name,avg((cumulative_present/cumulative_working)*100) as attendance_percent from student_class_mapping scm join cumulative_attendance_records car on scm.id=car.student_class_map_id where scm.class_id in (select class_id from student_class_mapping where class_id=1) group by 1;
+drop table absent_table
+
+drop table late_table
+
+update absent_table_final set absent_days=0 where absent_days is null
+
+create table late_table as (select student_class_map_id,concat(year(attendance_date),month(attendance_date)) as month_name,count(*) as late_days from student_attendance_map where status='L' group by 1,2 order by 1,2)
+
+create table late_table_final as (select t.student_class_map_id, t.month_name, att.late_days from temp t left OUTER join late_table att on t.student_class_map_id=att.student_class_map_id and t.month_name=att.month_name order by 1,2)
+
+update late_table_final set absent_days=0 where late_days is null
+
+create table attendance_records_final select wt.student_class_map_id,wt.month_name,pt.present_days,at.absent_days,lt.late_days,wt.working_days from present_table_final pt join working_table wt on pt.student_class_map_id=wt.student_class_map_id and wt.month_name=pt.month_name join absent_table_final at on pt.student_class_map_id=at.student_class_map_id and at.month_name=pt.month_name join late_table_final lt on pt.student_class_map_id=lt.student_class_map_id and lt.month_name=pt.month_name
+
+create table cumulative_attendance_records_final select ar.student_class_map_id,ar.month_name,sum(at.present_days) as cumulative_present,sum(at.absent_days) as cumulative_absent,sum(at.late_days) as cumulative_late,sum(at.working_days) as cumulative_working from attendance_records_final ar join attendance_records_final at on ar.student_class_map_id=at.student_class_map_id and ar.month_name>=at.month_name group by 1,2;
+
+drop table temp
+
+drop table absent_table
+
+drop table late_table
+
 
 
 
